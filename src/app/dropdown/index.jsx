a6b126ac33cc2616c9d6724366652829aa927c8e
bafir24 <bafirgenius@gmail.com>
2021-10-13 16:30:52 +0700

    mobile

--- src/app/dropdown/index.jsx
+++ src/app/dropdown/index.jsx
@@ -7,11 +7,16 @@ export default function Dropdown({ options, id, label, prompt, value, onChange }
   const ref = useRef(null);
 
   useEffect(() => {
-    document.addEventListener("click", close);
-    return () => document.removeEventListener("click", close);
+    ["click", "touchend"].forEach((e) => {
+      document.addEventListener(e, toggle);
+    });
+    return () =>
+      ["click", "touchend"].forEach((e) => {
+        document.removeEventListener(e, toggle);
+      });
   }, []);
 
-  function close(e) {
+  function toggle(e) {
     console.dir([e.target, ref.current]);
     setOpen(e && e.target === ref.current);
   }
@@ -26,9 +31,15 @@ export default function Dropdown({ options, id, label, prompt, value, onChange }
     return "";
   }
 
+  function selectOption(option) {
+    setQuery("");
+    onChange(option);
+    setOpen(false);
+  }
+
   return (
     <div className="dropdown">
-      <div className="control" onClick={() => setOpen((prev) => !prev)}>
+      <div className="control">
         <div className="selected-value">
           <input
             type="text"
@@ -39,22 +50,15 @@ export default function Dropdown({ options, id, label, prompt, value, onChange }
               setQuery(e.target.value);
               onChange(null);
             }}
-            onClick={() => setOpen((prev) => !prev)}
+            onClick={toggle}
+            onTouchEnd={toggle}
           />
         </div>
         <div className={`arrow ${open ? "open" : null}`} />
       </div>
       <div className={`options ${open ? "open" : null}`}>
         {filter(options).map((option) => (
-          <div
-            key={option[id]}
-            className={`option ${value === option ? "open" : "selected"}`}
-            onClick={() => {
-              setQuery("");
-              onChange(option);
-              setOpen(false);
-            }}
-          >
+          <div key={option[id]} className={`option ${value === option ? "selected" : null}`} onClick={() => selectOption(option)} onTouchEnd={() => selectOption(option)}>
             {option[label]}
           </div>
         ))}
